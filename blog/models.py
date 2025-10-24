6aeb2ea38413d7c4e6632c32e30602ca2f99b8d0
David <d11218700@gmail.com>
2025-10-24 12:04:21 +0600

    final project

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+#все что связанно с бд
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+    author = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='post_images/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def home(request):
+    posts = Post.objects.all().order_by('-date_posted')
+    return render(request, 'blog/home.html', {'posts': posts})
